b8e7645eb3d65b5334e10edfbcde103f5a8ab29c
Kenny <k-calhoun@live.com>
2025-12-21 14:43:37 -0500

    ui updates

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -4,8 +4,10 @@ import React from "react";
 import AppTheme from "../shared-theme/AppTheme";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
+
 import SideMenu from "./SideMenu";
 import Header from "./Header";
+import ChartUserByCountry from "./ChartCmaByCounty";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,6 +17,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
         <SideMenu />
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
           <Header />
+          <ChartUserByCountry />
           {children}
         </Box>
       </Box>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,13 @@
 
 
+
 import DashboardLayout from "../components/DashboardLayout";
 import MainGrid from "../components/MainGrid";
 
 export default function Dashboard() {
   return (
     <DashboardLayout>
+      <MainGrid />
       <MainGrid />
     </DashboardLayout>
   );
